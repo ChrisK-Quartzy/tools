537678d1e48b886c4783de1e7d1494764529fc49
Chris Koranda <chris.koranda@quartzy.com>
2021-11-09 16:13:18 -0600

    Add echos

--- tsshz.sh
+++ tsshz.sh
@@ -2,9 +2,11 @@
 
 case $1 in
     s-cat)
+        echo "ssh --> staging catalog"
         tsh ssh --cluster staging quartzy@stagingvpc_catalog0-c202f122-f9f0-5f9c-99e3-70e150ce0b2f
         ;;
     t-cat)
+        echo "ssh --> production catalog"
         tsh ssh --cluster productcion quartzy@prodvpc_catalog1-34d85505-1d2f-53e6-890b-19d66296c699
         ;;
 esac
