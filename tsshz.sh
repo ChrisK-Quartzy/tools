37796ae6a07191d501030db0210558a9252a6018
Chris Koranda <chris.koranda@quartzy.com>
2021-11-11 14:07:06 -0600

    Fix production catalog, add lab-api

--- tsshz.sh
+++ tsshz.sh
@@ -1,12 +1,20 @@
 #!/usr/bin/env zsh
 
 case $1 in
-    s-cat)
+    's-lab')
+        echo "ssh --> staging lab-api (rufio)"
+        tsh ssh --cluster staging quartzy@stagingvpc_rufio_php74
+        ;;
+    'p-lab')
+        echo "ssh --> production lab-api (rufio0)"
+        tsh ssh --cluster production quartzy@prodvpc_rufio0_php74
+        ;;
+    's-cat')
         echo "ssh --> staging catalog"
         tsh ssh --cluster staging quartzy@stagingvpc_catalog0-c202f122-f9f0-5f9c-99e3-70e150ce0b2f
         ;;
-    t-cat)
+    'p-cat')
         echo "ssh --> production catalog"
-        tsh ssh --cluster productcion quartzy@prodvpc_catalog1-34d85505-1d2f-53e6-890b-19d66296c699
+        tsh ssh --cluster production quartzy@prodvpc_catalog1-34d85505-1d2f-53e6-890b-19d66296c699
         ;;
 esac
