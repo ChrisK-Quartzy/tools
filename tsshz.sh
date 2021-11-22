1ecab5f56aa783dbbe629f79de28c3e3dcf65542
Chris Koranda <chris.koranda@quartzy.com>
2021-11-22 10:36:20 -0600

    Add LBE, fix staging worker target

--- tsshz.sh
+++ tsshz.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env zsh
 
 case $1 in
-    's-lab')
+    's-lapi')
         echo "ssh --> staging lab-api (rufio)"
         tsh ssh --cluster staging quartzy@stagingvpc_rufio_php74
         ;;
-    'p-lab')
+    'p-lapi')
         echo "ssh --> production lab-api (rufio0)"
         tsh ssh --cluster production quartzy@prodvpc_rufio0_php74
         ;;
@@ -18,11 +18,19 @@ case $1 in
         tsh ssh --cluster production quartzy@prodvpc_catalog1-34d85505-1d2f-53e6-890b-19d66296c699
         ;;
     's-work')
-        echo "ssh --> staging worker (abe0)"
-        tsh ssh --cluster production quartzy@stagingvpc_abe0-6769e7d4-3a8e-54a7-ae0f-9139f9d6e7fc
+        echo "ssh --> staging worker (aio0)"
+        tsh ssh --cluster staging quartzy@stagingvpc_aio0-a0ce475c-2c32-57de-b7e1-737709bc26fd
         ;;
     'p-work')
         echo "ssh --> production worker (worker0)"
         tsh ssh --cluster production quartzy@prodvpc_worker0-e3f054d8-9ef6-5f11-9d6f-df8b63610fd0
+        ;;
+    's-lbe')
+        echo "ssh --> staging lab-backend (abe0)"
+        tsh ssh --cluster staging quartzy@stagingvpc_abe0-6769e7d4-3a8e-54a7-ae0f-9139f9d6e7fc
+        ;;
+    'p-lbe')
+        echo "ssh --> production lab-backend (abe0)"
+        tsh ssh --cluster production quartzy@prodvpc_abe0-26f04faf-b232-54e4-b695-e4e634069b4e
         ;;
 esac
