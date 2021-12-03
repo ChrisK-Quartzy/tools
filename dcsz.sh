00acb4443e402cb14c36cf82641a7b87ec52a196
Chris Koranda <chris.koranda@quartzy.com>
2021-12-03 11:16:19 -0600

    Condense stop command

--- dcsz.sh
+++ dcsz.sh
@@ -2,7 +2,6 @@
 
 docker-compose --profile quartzy_no_workers up -d
 
-docker-compose stop public-api
-docker-compose stop webhooks
+docker-compose stop public-api webhooks
 
 docker-compose up -d vendor-frontend
